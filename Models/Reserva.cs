7b6a877db7bf69c1ee04fc469d08c839e9371056
josuemarcos <josue.fernandes@ee.ufcg.edu.br>
2025-10-06 11:23:16 -0300

    Ajusta método CalcularValorDiaria

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -37,16 +37,12 @@ namespace DesafioProjetoHospedagem.Models
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
 
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                valor *= 0.9M;
             }
 
             return valor;
