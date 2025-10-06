bdeb5445aa171c54ca2632e2198f9bcd0c00db01
josuemarcos <josue.fernandes@ee.ufcg.edu.br>
2025-10-06 11:09:06 -0300

    Ajusta método de cadastrar hospedes

---
+++ Models/Reserva.cs
@@ -0,0 +1,58 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(Pessoa hospede)
+        {
+            
+            if (Hospedes.Count < Suite.Capacidade)
+            {
+                Hospedes.Add(hospede);
+            }
+            else
+            {
+                throw new ArgumentException("O limite de pessoas para essa reserva já foi atingido!");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
+            // *IMPLEMENTE AQUI*
+            return 0;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            // TODO: Retorna o valor da diária
+            // Cálculo: DiasReservados X Suite.ValorDiaria
+            // *IMPLEMENTE AQUI*
+            decimal valor = 0;
+
+            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
+            // *IMPLEMENTE AQUI*
+            if (true)
+            {
+                valor = 0;
+            }
+
+            return valor;
+        }
+    }
+}
