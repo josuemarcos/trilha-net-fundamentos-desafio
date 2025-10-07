af56bc1ac4fa22c88442a2da667b526929cb01da
josuemarcos <josue.fernandes@ee.ufcg.edu.br>
2025-10-07 11:31:27 -0300

    Ajusta classe Nokia

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,15 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Nokia(string numero, string modeloFornecido, string imeiFornecido, int memoriaInformada) : base(numero, modeloFornecido, imeiFornecido, memoriaInformada)
+        {
+
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            System.Console.WriteLine($"Instalando {nomeApp} no celular {modelo}");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia = new("1234", "Nokia Lumia", "554422", 128);
+
+nokia.InstalarAplicativo("Whatsapp");
