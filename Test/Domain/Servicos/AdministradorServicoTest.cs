827f6238fff24b91646c5aa459505499617863b8
josuemarcos <josue.fernandes@ee.ufcg.edu.br>
2025-10-15 16:25:31 -0300

    Cria teste de salvar administrador no AdministradorServico

---
+++ Test/Domain/Servicos/AdministradorServicoTest.cs
@@ -0,0 +1,46 @@
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using MinimalApi.Dominio.Entidades;
+using MinimalApi.Dominio.Servicos;
+using MinimalApi.Infraestrutura.Db;
+
+namespace Test;
+
+[TestClass]
+public class AdministradorServicoTest
+{
+    private DbContexto CriarContextoDeTestes()
+    {
+        var assemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        var path = Path.GetFullPath(Path.Combine(assemblyPath ?? "", "..", "..", ".."));
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(path ?? Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables();
+        var configuration = builder.Build();
+
+        return new DbContexto(configuration);
+    }
+    [TestMethod]
+    public void TestarSalvarAdministrador()
+    {
+        //Arrange
+        var contexto = CriarContextoDeTestes();
+        contexto.Database.ExecuteSqlRaw("TRUNCATE TABLE Administradores");
+        
+        var adm = new Administrador();
+        adm.Email = "teste@teste.com";
+        adm.Senha = "teste";
+        adm.Perfil = "adm";
+
+        var administradorServico = new AdministradorServico(contexto);
+        //Act
+        administradorServico.Incluir(adm);
+
+
+        //Assert
+        Assert.AreEqual(1, administradorServico.Todos(1).Count);
+
+    }
+}
