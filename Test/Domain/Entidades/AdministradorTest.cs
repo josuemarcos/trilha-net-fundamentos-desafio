fd08544e4b6041c316b7efdc6efd0f7fb782213a
josuemarcos <josue.fernandes@ee.ufcg.edu.br>
2025-10-15 14:23:02 -0300

    Cria teste de propriedades de Administrador

---
+++ Test/Domain/Entidades/AdministradorTest.cs
@@ -0,0 +1,28 @@
+using MinimalApi.Dominio.Entidades;
+
+namespace Test;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestarGetSetPropriedades()
+    {
+        //Arrange
+        var adm = new Administrador();
+
+        //Act
+        adm.Id = 1;
+        adm.Email = "teste@teste.com";
+        adm.Senha = "teste";
+        adm.Perfil = "adm";
+
+
+        //Assert
+        Assert.AreEqual(1, adm.Id);
+        Assert.AreEqual("teste@teste.com", adm.Email);
+        Assert.AreEqual("teste", adm.Senha);
+        Assert.AreEqual("adm", adm.Perfil);
+
+    }
+}
